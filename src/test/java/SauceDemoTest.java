9697ed61fdfce00d5b001c106de32d43125a9ecd
nikitam <nikita.milka@citadele.lv>
2024-06-17 20:41:26 +0300

    [master]: Remove unused import

--- src/test/java/SauceDemoTest.java
+++ src/test/java/SauceDemoTest.java
@@ -1,7 +1,6 @@
 import lv.acodemy.page_object.LoginPage;
 import lv.acodemy.page_object.ProductsPage;
 import lv.acodemy.utils.Config;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.slf4j.Logger;
